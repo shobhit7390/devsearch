d3c5f5d919e86a4f93fd4511ad44663b04f65379
shobhit7390 <vms6402@gmail.com>
2023-10-02 10:31:39 +0530

    Signals in Django

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 import uuid
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 class Profile(models.Model):
@@ -23,7 +24,7 @@ class Profile(models.Model):
     id=models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 
 class Skill(models.Model):
@@ -36,3 +37,6 @@ class Skill(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+
