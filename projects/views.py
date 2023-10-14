6c856d17cca959cbf2b09743713f1a7f5498daa4
shobhit7390 <vms6402@gmail.com>
2023-10-14 21:29:26 +0530

    Handling reviews

--- projects/views.py
+++ projects/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.contrib import messages
 
 from .utils import searchProjects, paginateProjects
 from .models import Project, Tag
-from .forms import ProjectForm 
+from .forms import ProjectForm, ReviewForm
 
 
 
@@ -20,7 +21,21 @@ def projects(request):
 
 def project(request,pk):
     projectObj=Project.objects.get(id=pk)
-    return render(request, 'projects/single-project.html', {'project':projectObj})
+    form=ReviewForm()
+
+    if request.method=='POST':
+        form=ReviewForm(request.POST)
+        review=form.save(commit=False)
+        review.project=projectObj
+        review.owner=request.user.profile
+        review.save()
+
+        projectObj.getVoteCount
+        
+        messages.success(request, 'Your review was successfully submitted!')
+        return redirect('project', pk=projectObj.id)
+
+    return render(request, 'projects/single-project.html', {'project':projectObj, 'form':form})
 
 
 @login_required(login_url="login")
