979f08622f7f824a3f46b0a02f8b2849524e2731
shobhit7390 <vms6402@gmail.com>
2023-10-15 21:45:12 +0530

    Worling with Messages

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.db.models import Q
-from .models import Profile, Skill
+from .models import Profile, Skill, Message
 from django.contrib.auth.decorators import login_required
 
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm 
@@ -19,7 +19,7 @@ def loginUser(request):
         return redirect('profiles')
     
     if request.method == 'POST':
-        username=request.POST['username'].lower()
+        username=request.POST['username']
         password=request.POST['password']
 
         try:
@@ -146,6 +146,7 @@ def updateSkill(request, pk):
     return render(request, 'users/skill_form.html', context)
 
 
+@login_required(login_url='login')
 def deleteSkill(request, pk):
     profile=request.user.profile
     skill=profile.skill_set.get(id=pk)
@@ -156,3 +157,31 @@ def deleteSkill(request, pk):
     
     context={'object':skill}
     return render(request, 'delete_template.html', context)
+
+
+@login_required(login_url='login')
+def inbox(request):
+    profile=request.user.profile
+    messageRequests=profile.messages.all()
+    unreadCount=messageRequests.filter(is_read=False).count()
+    
+    context={'messageRequests':messageRequests, 'unreadCount':unreadCount}
+    return render(request, 'users/inbox.html', context)
+
+
+@login_required(login_url='login')
+def viewMessage(request, pk):
+    profile=request.user.profile
+    message=profile.messages.get(id=pk)
+    if message.is_read == False:
+        message.is_read = True
+        message.save()
+
+    context={'message':message}
+    return render(request, 'users/message.html', context)
+
+
+def createMessage(request, pk):
+    recipient=Profile.objects.get(id=pk)
+    context={'recipient':recipient}
+    return render(request, 'users/message_form.html', context)
