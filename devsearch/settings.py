4ed6152adef189cf14b3b96dfc8a965219df2868
shobhit7390 <vms6402@gmail.com>
2023-10-18 11:12:53 +0530

    Sending Welcome email

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -125,8 +125,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER='vms6402@gmail.com'
-EMAIL_HOST_PASSWORD='ogxu cmzg drjh tada'
+EMAIL_HOST_USER='Email'
+EMAIL_HOST_PASSWORD='Password'
 
 
 # Static files (CSS, JavaScript, Images)
